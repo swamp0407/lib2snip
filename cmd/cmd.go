e52d6ad425edb567bcb5a7a4321f5be86244658f
Tatsuya <swamp0407@gmail.com>
2023-02-01 10:25:48 +0900

    refactoring cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,44 +8,11 @@ import (
 	"path/filepath"
 
 	"github.com/swamp0407/lib2snip/config"
-	"github.com/swamp0407/lib2snip/entities"
 
 	"strings"
 )
 
-func genSnippets(scope string, files []config.File, configPath string) []entities.Snippet {
-	var snippets []entities.Snippet
-	for _, file := range files {
-		if !strings.HasPrefix(file.Path, "/") {
-			file.Path = filepath.Join(filepath.Dir(configPath), file.Path)
-			buf, err := os.ReadFile(file.Path)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-
-			snippet := entities.Snippet{
-				Name:        file.Name,
-				Prefix:      file.Prefix,
-				Scope:       scope,
-				Description: file.Description,
-				Body:        string(buf),
-			}
-			snippets = append(snippets, snippet)
-		}
-	}
-	return snippets
-}
-
-func genSnippetsWithScope(c config.Config) map[string][]entities.Snippet {
-	snippetsWithScope := map[string][]entities.Snippet{}
-	for scope, files := range c.Scope {
-		snippets := genSnippets(scope, files, c.ConfigFile)
-		snippetsWithScope[scope] = snippets
-	}
-	return snippetsWithScope
-}
-func outputJson(snippetsWithScope map[string][]entities.Snippet, filename string) error {
+func outputJson(v config.VsOutput, filename string) error {
 	f, err := os.Create(filename)
 	if err != nil {
 		return err
@@ -53,40 +20,7 @@ func outputJson(snippetsWithScope map[string][]entities.Snippet, filename string
 	defer f.Close()
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "  ")
-	var Vssnippets = map[string]VsSnip{}
-	for _, snippets := range snippetsWithScope {
-		for _, snippet := range snippets {
-			vssnippet := VsSnip{
-				Prefix:      snippet.Prefix,
-				Body:        snippet.Body,
-				Description: snippet.Description,
-				Scope:       snippet.Scope,
-			}
-			Vssnippets[snippet.Name] = vssnippet
-		}
-	}
-
-	return enc.Encode(Vssnippets)
-}
-
-//	func outputYaml(snippetsWithScope map[string][]entities.Snippet, filename string) error {
-//		panic("not implemented")
-//	}
-type VsSnip struct {
-	Prefix      string `json:"prefix"`
-	Body        string `json:"body"`
-	Description string `json:"description"`
-	Scope       string `json:"scope"`
-}
-
-func outputSnippets(snippetsWithScope map[string][]entities.Snippet, filename string) error {
-	if strings.HasSuffix(filename, ".code-snippets") {
-		return outputJson(snippetsWithScope, filename)
-	}
-	//  else if strings.HasSuffix(filename, ".yaml") || strings.HasSuffix(filename, ".yml") {
-	// 	return outputYaml(snippetsWithScope, filename)
-	// }
-	return fmt.Errorf("output file must be .code-snippets")
+	return enc.Encode(v)
 }
 
 func ask4confirm() bool {
@@ -107,44 +41,58 @@ func ask4confirm() bool {
 	return false
 }
 
-func Run() {
-	flag.Parse()
-	configFile := flag.Lookup("c").Value.String()
+func hasValidSuffixV(filename string) bool {
+	return filepath.Ext(filename) == ".code-snippets"
+}
 
-	c, err := config.ParseConfigFile(configFile)
-	if err != nil {
-		panic(err)
-	}
-
-	snippetsWithScope := genSnippetsWithScope(c)
-
+func processOFlag() string {
 	oflag := flag.Lookup("o").Value.String()
-
 	filedir := filepath.Dir(oflag)
 	filebase := filepath.Base(oflag)
-	if !strings.HasSuffix(filebase, ".code-snippets") {
-		fmt.Println("output file must be .code-snippets")
-		os.Exit(1)
-	}
+
 	if filebase == ".code-snippets" {
 		filebase = "snippets.code-snippets"
 	}
-	var filename string
+	if !hasValidSuffixV(oflag) {
+		filebase = filebase + ".code-snippets"
+	}
+
 	var splitFiledir []string = strings.Split(filedir, "/")
 	if splitFiledir[len(splitFiledir)-1] != ".vscode" {
 		filedir = filepath.Join(filedir, ".vscode")
 	}
 	if _, err := os.Stat(filedir); os.IsNotExist(err) {
-		fmt.Printf("dir %s not exist, Can create it?", filepath.Dir(filename))
+		fmt.Printf("dir %s not exist, Can create it?", filedir)
 		if ask4confirm() {
-			os.MkdirAll(filepath.Dir(filename), 0755)
+			os.MkdirAll(filedir, 0755)
 		} else {
 			os.Exit(1)
 		}
 	}
-	filename = filepath.Join(filedir, filebase)
+	filename := filepath.Join(filedir, filebase)
+	if _, err := os.Stat(filename); err == nil {
+		fmt.Printf("file %s exist, Can overwrite it?", filename)
+		if !ask4confirm() {
+			os.Exit(1)
+		}
+	}
+	return filename
+}
 
-	outputSnippets(snippetsWithScope, filename)
+func Run() {
+	flag.Parse()
+	configFile := flag.Lookup("c").Value.String()
+	c := config.NewConfig(configFile)
+
+	if err := c.ParseConfigFile(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	v := c.GenVsOutput()
+	filename := processOFlag()
+	outputJson(v, filename)
+
 }
 
 func init() {
--- config/config.go
+++ config/config.go
@@ -4,120 +4,151 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/swamp0407/lib2snip/entities"
 	"gopkg.in/yaml.v2"
 )
 
-func parseYaml(configFile string) (map[string]interface{}, error) {
-	buf, err := os.ReadFile(configFile)
+func (c *Config) parseYamlConfig() error {
+	buf, err := os.ReadFile(c.ConfigFile)
 	if err != nil {
 		panic(err)
 	}
-	var config map[string]interface{}
-	if err = yaml.Unmarshal(buf, &config); err != nil {
-		return nil, errors.New("error parsing config file")
+	if err = yaml.Unmarshal(buf, c); err != nil {
+		return errors.New("error parsing Yaml config file")
 	}
-	return config, nil
+	return nil
 }
 
-func parseJson(configFile string) (map[string]interface{}, error) {
+func (c *Config) parseJsonConfig() error {
 	panic("not implemented")
 }
 
 type File struct {
-	Name        string
-	Path        string
-	Prefix      string
-	Description string
+	Name        string      `yaml:"name"`
+	Path        string      `yaml:"path"`
+	Prefix      interface{} `yaml:"prefix"` // string or []string
+	Description string      `yaml:"description"`
 }
-
-// var _ = File{name: "a", path: "b", prefix: "c", description: "d"}
+type Scope = map[string][]File
 
 type Config struct {
-	Scope      map[string][]File
+	File       Scope `yaml:"file"`
 	ConfigFile string
 }
 
-var _ = ParseConfigFile
+type VsOutput = map[string][]entities.Snippet
 
-func ParseConfigFile(configFile string) (Config, error) {
-	if strings.HasSuffix(configFile, ".yaml") || strings.HasSuffix(configFile, ".yml") {
-		config, err := parseYaml(configFile)
-		if err != nil {
-			return Config{}, err
-		}
-		if config["file"] == nil {
-			return Config{}, fmt.Errorf("file key not found in config file")
-		}
-		file := config["file"].(map[interface{}]interface{})
-		if file["scope"] == nil {
-			return Config{}, fmt.Errorf("scope key not found in config file")
-		}
-		scopes := file["scope"].(map[interface{}]interface{})
-		var configScope = map[string][]File{}
-		for k, v := range scopes {
-			scope := k.(string)
-			vv := v.([]interface{})
-			var files []File
-			for _, vvv := range vv {
-				vvvv := vvv.(map[interface{}]interface{})
-				var prefix string
-				var name string
-				var path string
-				var description string
-				if vvvv["name"] == nil {
-					return Config{}, fmt.Errorf("name key not found in config file")
-				}
-				name = vvvv["name"].(string)
+func NewConfig(configFile string) Config {
+	return Config{ConfigFile: configFile}
+}
 
-				switch vvvv["prefix"].(type) {
-				case string:
-					prefix = vvvv["prefix"].(string)
-				case []interface{}:
-					prefixL := vvvv["prefix"].([]interface{})
-					var prefixL2 []string
-					for _, v := range prefixL {
-						prefixL2 = append(prefixL2, v.(string))
-					}
-					prefix = strings.Join(prefixL2, ",")
-				default:
-					prefix = name
-				}
-
-				if vvvv["path"] == nil {
-					return Config{}, fmt.Errorf("path key not found in config file")
-				}
-				path = vvvv["path"].(string)
-
-				if vvvv["description"] == nil {
-					description = ""
-				}
-				description = vvvv["description"].(string)
-
-				f := File{
-					Name:        name,
-					Path:        path,
-					Prefix:      prefix,
-					Description: description,
-				}
-				// fmt.Println(scope, f)
-				files = append(files, f)
-				// fmt.Println(files)
-			}
-			configScope[scope] = files
-
-		}
-		// fmt.Println(configScope)
-
-		return Config{Scope: configScope, ConfigFile: configFile}, nil
-	} else if strings.HasSuffix(configFile, ".json") {
-		config, err := parseJson(configFile)
-		if err != nil {
-			return Config{}, err
-		}
-		_ = config
-		return Config{}, nil
+func (c *Config) ParseConfigFile() error {
+	var err error
+	if c.ConfigFile == "" {
+		return fmt.Errorf("config file is required")
 	}
-	return Config{}, fmt.Errorf("config file must be yaml")
+
+	if strings.HasSuffix(c.ConfigFile, ".yaml") || strings.HasSuffix(c.ConfigFile, ".yml") {
+		err = c.parseYamlConfig()
+	} else if strings.HasSuffix(c.ConfigFile, ".json") {
+		err = c.parseJsonConfig()
+	} else {
+		return fmt.Errorf("config file must be yaml or json")
+	}
+	return err
+}
+
+func validateAndEvaluatePrefix(prefix_i interface{}) (string, error) {
+	var prefix string
+	switch prefix_i := prefix_i.(type) {
+	case string:
+		prefix = prefix_i
+	case []interface{}:
+		prefixList := make([]string, 0)
+		for _, v := range prefix_i {
+			if _, ok := v.(string); !ok {
+				return "", errors.New("prefix is not valid")
+			}
+
+			prefixList = append(prefixList, v.(string))
+		}
+		prefix = strings.Join(prefixList, ",")
+	default:
+		return "", nil
+	}
+	return prefix, nil
+}
+
+func readFromPath(path string) (*[]byte, error) {
+	buf, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return &buf, nil
+}
+
+func (c *Config) customReadFromPath(path string) (*[]byte, error) {
+	if !strings.HasPrefix(path, "/") {
+		path = filepath.Join(filepath.Dir(c.ConfigFile), path)
+	}
+	return readFromPath(path)
+
+}
+
+func (c *Config) convertFile2Snippet(file File) (*entities.Snippet, error) {
+	var prefix string
+	var name = file.Name
+	var description = file.Description
+	var path = file.Path
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+	if path == "" {
+		return nil, errors.New("path is required")
+	}
+	fmt.Println("path: ", path)
+	buf, err := c.customReadFromPath(path)
+	if err != nil {
+		return nil, err
+	}
+	prefix, err = validateAndEvaluatePrefix(file.Prefix)
+	if err != nil {
+		return nil, errors.New("prefix is not valid")
+	}
+	if prefix == "" {
+		prefix = name
+	}
+
+	snippet := entities.Snippet{
+		Name:        name,
+		Body:        string(*buf),
+		Prefix:      prefix,
+		Description: description,
+	}
+	return &snippet, nil
+}
+
+func (c *Config) genSnippets(files []File) []entities.Snippet {
+	var snippets []entities.Snippet
+	for _, file := range files {
+		snippet, err := c.convertFile2Snippet(file)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		snippets = append(snippets, *snippet)
+	}
+	return snippets
+}
+
+func (c *Config) GenVsOutput() VsOutput {
+	output := make(VsOutput)
+	for scope, files := range c.File {
+		snippets := c.genSnippets(files)
+		output[scope] = snippets
+	}
+	return output
 }
